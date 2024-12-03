5c78df93b0afe91200081fbae5bff6c371c217d1
DaHub49 <dyls.martin.49@gmail.com>
2024-12-03 10:45:06 +0200

    03/12/2024 @10:44: End of Day 4: video 72

--- app/src/main/java/za/co/howtogeek/bankaccountprogram/BankAccount.kt
+++ app/src/main/java/za/co/howtogeek/bankaccountprogram/BankAccount.kt
@@ -1,18 +1,49 @@
 package za.co.howtogeek.bankaccountprogram
 
-class BankAccount(var accountHolder: String, var balance: Double){
+import kotlin.concurrent.fixedRateTimer
+
+class BankAccount(
+    var accountHolder: String,
+    var balance: Double)
+{
 
     private val transactionHistory = mutableListOf<String>()
 
     fun deposit(amount: Double){
-
+        balance += amount
+        transactionHistory.add("$accountHolder deposited R$amount.") // \nNew balance: R$balance.")
     }
 
     fun withdraw(amount: Double){
+        if (amount <= balance){
+            // We can withdraw
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew R$amount.\nNew balance: R$balance.")
+        } else {
+            // We can't withdraw the money
+            println("Insufficient funds to withdraw R$amount.")
+        }
 
     }
 
     fun displayTransactionHistory(){
+        println("Transaction history for $accountHolder")
+        // Without index:
+        for (transaction in transactionHistory) {
+            println(transaction)
+        }
+        println("____________________________________________\nBalance:\n$balance")
 
+
+
+
+        /* With index:
+        if (transactionHistory.size > 0){
+            for (index in 0 until transactionHistory.size){
+                println(transactionHistory.get(index))
+            }
+        } else
+            println("No Transaction history.")
+         */
     }
 }
--- app/src/main/java/za/co/howtogeek/bankaccountprogram/BankMain.kt
+++ app/src/main/java/za/co/howtogeek/bankaccountprogram/BankMain.kt
@@ -1,8 +1,15 @@
 package za.co.howtogeek.bankaccountprogram
 
 fun main(){
-
     val dilshanBankAccount = BankAccount("Dilshan Tillakaratne", 31037.37)
 
-    println("dilshanBankAccount.accountHolder: ${dilshanBankAccount.accountHolder}")
+    //println("dilshanBankAccount.accountHolder: ${dilshanBankAccount.accountHolder}")
+
+    dilshanBankAccount.deposit(200.0)
+    dilshanBankAccount.withdraw(1200.00)
+    dilshanBankAccount.deposit(3000.00)
+    dilshanBankAccount.deposit(2000.00)
+    dilshanBankAccount.withdraw(3333.15)
+
+    dilshanBankAccount.displayTransactionHistory()
 }
